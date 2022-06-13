0a10fb57f74b35f45781cd4b0b55dd72ef2339b9
singhalpunit07 <107442292+singhalpunit07@users.noreply.github.com>
2022-06-14 01:22:57 +0200

    Update streamlit_app.py
    error updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ import streamlit;
 streamlit.title ("my parents");
 streamlit.header("breakfast Menu");
 streamlit.text ("Omega");
-streamlit.test ("kale, Spinach");
+streamlit.text ("kale, Spinach");
