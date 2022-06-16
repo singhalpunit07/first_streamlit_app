9b0500440960da733f56ba6b1226c06342029d8d
singhalpunit07 <107442292+singhalpunit07@users.noreply.github.com>
2022-06-16 15:43:30 +0200

    Update streamlit_app.py
    updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit;
 
 streamlit.title ("my parents");
-streamlit.header("breakfast Menu");
-streamlit.text ("Omega");
-streamlit.text ("kale, Spinach");
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
