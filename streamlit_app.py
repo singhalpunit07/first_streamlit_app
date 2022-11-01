a60473771e6842d43ace99d36d6fd83dc48bd2b0
singhalpunit07 <107442292+singhalpunit07@users.noreply.github.com>
2022-11-01 15:14:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@
 import streamlit;
 import pandas;
 
-streamlit.title ("my parents");
+streamlit.title ("My Parents new Healthy Dinner");
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
